5659832ca7b1bcf231f3861c11dd8dcc0a731459
Clara <144264170+clara-tyman@users.noreply.github.com>
2023-10-17 13:09:29 -0400

    linter fix 2

--- main.ts
+++ main.ts
@@ -5,46 +5,46 @@
  * This program reads the light level then diplasys by lighting LED's on neopixels
 */
 
-  let lightLevel: number
-  lightLevel = input.lightLevel()
-  let neopixelStrip: neopixel.Strip = null
+let lightLevel: number
+lightLevel = input.lightLevel()
+let neopixelStrip: neopixel.Strip = null
 
-  basic.clearScreen()
-  basic.showIcon(IconNames.Happy)
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
 
-  // cleanup
-  neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-  neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-  neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-  neopixelStrip.show()
+// cleanup
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
 
 input.onButtonPressed(Button.A, function () {
   // get light level
   if (lightLevel <= 51) {
-  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-  neopixelStrip.show()
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
   }
   if (lightLevel >= 52) {
-  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
-  neopixelStrip.show()
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+    neopixelStrip.show()
   }
   if (lightLevel > 104) {
-  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
-  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
-  neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
-  neopixelStrip.show()
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+    neopixelStrip.show()
   }
   if (lightLevel > 156) {
-  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
-  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
-  neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
-  neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
-  neopixelStrip.show()
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+    neopixelStrip.show()
   }
   if (lightLevel > 208) {
-  neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
-  neopixelStrip.show()
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+    neopixelStrip.show()
   }
 })
